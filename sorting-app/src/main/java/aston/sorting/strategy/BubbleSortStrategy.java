3ed11b999134b538b3e44f019296c522199ad479
Артур Силкин <mrwhite19841@gmail.com>
2025-11-14 16:43:06 +0600

    Made some minor edits

--- sorting-app/src/main/java/aston/sorting/strategy/BubbleSortStrategy.java
+++ sorting-app/src/main/java/aston/sorting/strategy/BubbleSortStrategy.java
@@ -7,16 +7,16 @@ public class BubbleSortStrategy<T> implements SortingStrategy<T>
 
     @Override
     public void sort(T[] array, Comparator<T> comparator) {
-        if(array.equals(null) || array.length <= 1)
+        if(array == null || array.length <= 1)
             return;
 
-        int lenght = array.length;
+        int length = array.length;
         boolean flag;
 
-        for(int i = 0; i < lenght-1; i++)
+        for(int i = 0; i < length-1; i++)
         {
             flag = false;
-            for (int j = 0; j < lenght - i - 1; j++)
+            for (int j = 0; j < length - i - 1; j++)
             {
                 if(comparator.compare(array[j], array[j+1]) > 0)
                 {
